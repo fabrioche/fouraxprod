272c87b9d50c620036a1958779fed07196787548
fabrioche <f.dasilva75@gmail.com>
2013-07-18 16:47:54 +0200

    5 ème commit : probleme fonction add_user

--- register.php
+++ register.php
@@ -1,28 +1,26 @@
 <?php
-
+session_start();
 require_once("req_connect.php");
 require ("functions.php");
-if (isset($_POST["username"])) {
-    $username = nl2br($_POST["username"]);
-} else {
-    $username = NULL;
-}
-if (isset($_POST["email"])) {
-    $email = nl2br($_POST["email"]);
-} else {
-    $email = NULL;
-}
-if (isset($_POST["password"])){
-     $mdp = create_hash($_POST["password"]);
-} 
 
-
-
+//$username = isset($_POST['username']);
+//$email = isset($_POST['email']);
+//$mdp = create_hash(isset($_POST['mdp']));
 
 if (isset($_POST["ok"])) {
-   $req = mysqli_query($bdd,"INSERT INTO compte VALUES('', '$username', '$email', '$mdp')");
-   
+    $username = isset($_POST['username']);
+    $email = isset($_POST['email']);
+    $mdp = create_hash(isset($_POST['mdp']));
+
+    if (add_user($bdd,$username,$mdp) === false) {
+
+        echo "Ce username existe déjà";
+    } else {
+        $_SESSION['username'] = $username;
+        echo "Bonjour $username";
+    }
 }
+//echo add_user($bdd, 'fabrioche', 'toto');
 ?>
 <!DOCTYPE html>
 <html>
@@ -34,23 +32,22 @@ if (isset($_POST["ok"])) {
     </head>
     <body>
         <div class="container">
-        <div class="navbar">
-            <div class="navbar-inner">
-                <ul class="nav">
-                    <li class="brand">Fouraxprod</li>
-                    <li class="active"><a href="index.php">Home</a></li>
-                    <li><a href="#">Link</a></li>
-                </ul>
+            <div class="navbar">
+                <div class="navbar-inner">
+                    <ul class="nav">
+                        <li class="brand">Fouraxprod</li>
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        
             <div class='row'>
                 <div class='span5'>
                     <form class="form-signin " action="register.php" method="post">
                         <h2 class="form-signin-heading">Please sign in</h2>
                         <label for="username"><input type="text" class="input-block-level" id="username" name="username" placeholder="Username" size='50' /></label>
                         <label for="email"><input type="email" class="input-block-level" id="email" name="email" placeholder="Email" size='50'/></label>
-                        <label for="password"><input type="password" class="input-block-level" id="password" name="password" placeholder="Password" size='50'/></label>
+                        <label for="password"><input type="password" class="input-block-level" id="mdp" name="mdp" placeholder="Password" size='50' required/></label>
                         <button class="btn btn-large btn-primary" type="submit" id="ok" name="ok">Sign in</button>
                     </form>
                 </div>

7c0ec444dcc9cfdf50c0de5a81fe3b06e1e7f9e1
fabrioche <f.dasilva75@gmail.com>
2013-07-20 17:19:52 +0200

    8 ème commit : modification de la page fiche.php ( ajout d un foreach pour afficher la liste des films ; NON DEFINITIF)

--- fiche.php
+++ fiche.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+require ("req_connect.php");
+
+$req = mysqli_query($bdd, "SELECT nom_genre FROM genre") or die(mysqli_error());
+
+while ($row = mysqli_fetch_assoc($req)) {
+    $resut_ligne[] = $row;
+}
+//    $nom_genre=$row["nom_genre"];
+//    $id_genre=$row["id_genre"];
+//    for ($i =0;  $i<= 15 ; $i++)
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -14,71 +27,56 @@
                 <div class="navbar-inner">
                     <li class="brand">Fouraxprod</li>
                     <ul class="nav">
-                        <li class="active"><a href="#">Home</a></li>
+                        <li class="active"><a href="index.php">Home</a></li>
                     </ul>
                     <ul class="nav pull-right">
                         <li class="active"><a  href="register.php">Se déconnecter</a></li>
                     </ul>
                 </div>
             </div>
-           
-            <div class="text-center">
-                
+            <div >
                 <form>
-                    
                     <fieldset>
                         <legend>Fiche du Film</legend>
                         <label>Titre</label>
                         <input type="text" placeholder="Type something…">
                         <label>Genre</label>
-                            <!-- Button to trigger modal -->
-    <a href="#myModal" role="button" class="btn" data-toggle="modal">Genre du film</a>
-     
-    <!-- Modal -->
-    <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h3 id="myModalLabel">Catégories</h3>
-    </div>
-    <div class="modal-body">
-<!--    <p>One fine body…</p>-->
-              
-           
-<input type="radio" name="fav_food" id="food1" value="Pizza">
-<label for="food1">Pizza</label> <br />
-<input type="radio" name="fav_food" id="food2" value="Cereal">
-<label for="food2">Cereal</label> <br />
-<input type="radio" name="fav_food" id="food3" value="Ramen Noodles">
-<label for="food3">Ramen Noodles</label> <br />
-<input type="radio" name="fav_food" id="food4" value="Hamburger">
-<label for="food4">Hamburger</label>
-            
-      
-    
-    
-      
-      
-        
-    </div>
-    <div class="modal-footer">
-    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-    <button class="btn btn-primary">Save changes</button>
-    </div>
-    </div>
+                        <!-- Button to trigger modal -->
+                        <a href="#myModal" role="button" class="btn" data-toggle="modal">Genre du film</a>
+                        <!-- Modal -->
+                        <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                <h3 id="myModalLabel">Catégories</h3>
+                            </div>
+                            <div class="modal-body">
+                        <!--    <p>One fine body…</p>-->
+                                <div class="span3  ">
+                                    <?php
+                                    foreach ($resut_ligne as $row) {
+                                        echo '<label for="genre" class="radio text-left">
+                                        <input type="radio" name="nom_genre" id="genre" value="">' . $row["nom_genre"] . '
+                                    </label>';
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                                <button class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
                         <label>Année</label>
                         <input type="text" placeholder="11/07/2013">
                         <label>Résumé</label>
-                        <textarea rows="3"></textarea>
+                        <textarea rows="3" name="resume"></textarea>
                         <label>Commentaire</label>
-                        <textarea rows="3"></textarea>
-                        <span class="help-block">Example block-level help text here.</span>
+                        <textarea rows="3" name="commentaire"></textarea>
+                        <label>Réalisateur</label>
                         <button type="submit" class="btn">Submit</button>
                     </fieldset>
                 </form>
             </div>
-                        
-
         </div>
-    
-</body>
+    </body>
 </html>

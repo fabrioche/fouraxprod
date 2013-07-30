3a09a88030a6982eaad3fa8974dafed5c0111c1f
fabrioche <f.dasilva75@gmail.com>
2013-07-30 17:10:51 +0200

    9 ème commit : modification de la page fiche.php (récupération de l id_compte et de l id_genre pour l INSERT dans la table film) . PROBLEME: Warning: Invalid argument supplied for foreach() in C:\wamp2\www\fouraxprod\fiche.php on line 76 après insertion

--- fiche.php
+++ fiche.php
@@ -2,14 +2,31 @@
 session_start();
 require ("req_connect.php");
 
-$req = mysqli_query($bdd, "SELECT nom_genre FROM genre") or die(mysqli_error());
+$id_compte = $_SESSION["id_compte"];
+$req = mysqli_query($bdd, "SELECT * FROM genre") or die(mysqli_error($bdd));
+$row = mysqli_fetch_all($req);
+if (isset($_POST["ok"])) {
 
-while ($row = mysqli_fetch_assoc($req)) {
-    $resut_ligne[] = $row;
+
+    $titre = $_POST['titre'];
+    $resume = $_POST['resume'];
+    $commentaire = $_POST['commentaire'];
+    $note = $_POST['note'];
+    $id_genre = $_POST["id_genre"];
+    $annee = $_POST['annee'];
+    $req = mysqli_query($bdd, "INSERT INTO film (titre, resume, commentaire, note, annee, id_compte, id_genre)
+        VALUES ('$titre', '$resume', '$commentaire', '$note', '$annee', '$id_compte', '$id_genre')") or die(mysqli_error($bdd));
+    $last_id_film = mysqli_insert_id($bdd);
+
+    $req1 = mysqli_query($bdd, "INSERT INTO personne(nom)
+        VALUES ('$nom')") or die(mysqli_error($bdd));
 }
-//    $nom_genre=$row["nom_genre"];
-//    $id_genre=$row["id_genre"];
-//    for ($i =0;  $i<= 15 ; $i++)
+//Si une image a été envoyée par le formulaire :
+if (isset($_FILES["nom_media"])) {
+    //Upload de l'image dans le répertoire ./photo
+    $title = basename($_FILES['nom_media']['name']);
+    $resultat = move_uploaded_file($_FILES['nom_media']['tmp_name'], "../photo/$title");
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,11 +52,13 @@ while ($row = mysqli_fetch_assoc($req)) {
                 </div>
             </div>
             <div >
-                <form>
-                    <fieldset>
-                        <legend>Fiche du Film</legend>
+                <form action="fiche.php" method="post" enctype="multipart/form-data">
+
+
+                    <legend>Fiche du Film</legend>
+                    <div class="span6">
                         <label>Titre</label>
-                        <input type="text" placeholder="Type something…">
+                        <input type="text" placeholder="Type something…" name="titre">
                         <label>Genre</label>
                         <!-- Button to trigger modal -->
                         <a href="#myModal" role="button" class="btn" data-toggle="modal">Genre du film</a>
@@ -53,9 +72,9 @@ while ($row = mysqli_fetch_assoc($req)) {
                         <!--    <p>One fine body…</p>-->
                                 <div class="span3  ">
                                     <?php
-                                    foreach ($resut_ligne as $row) {
+                                    foreach ($req as $row) {
                                         echo '<label for="genre" class="radio text-left">
-                                        <input type="radio" name="nom_genre" id="genre" value="">' . $row["nom_genre"] . '
+                                        <input type="radio" name="id_genre" id="genre" value="' . $row["id_genre"] . '">' . $row["nom_genre"] . '
                                     </label>';
                                     }
                                     ?>
@@ -67,14 +86,53 @@ while ($row = mysqli_fetch_assoc($req)) {
                             </div>
                         </div>
                         <label>Année</label>
-                        <input type="text" placeholder="11/07/2013">
+                        <input type="text" placeholder="11/07/2013" name="annee"/>
+                        <label for="icone">Photo :</label>
+                        <input type="file" name="img_film" id="img_film" />
                         <label>Résumé</label>
                         <textarea rows="3" name="resume"></textarea>
                         <label>Commentaire</label>
                         <textarea rows="3" name="commentaire"></textarea>
+                    </div>
+                    <div class="span5">
                         <label>Réalisateur</label>
-                        <button type="submit" class="btn">Submit</button>
-                    </fieldset>
+                        <input type="text" name="nom" size="50"/>
+                        <label>Scénariste</label>
+                        <input type="text" name="nom" size="50"/>
+                        <label>Acteur principal </label>
+                        <input type="text" size="50" name="nom"/>
+                        <label for="icone">Photo :</label>
+                        <input type="file" name="img_acteur" id="img_acteur" />
+                        <div class="accordion-heading">
+                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+                                Autres acteurs
+                            </a>
+                        </div>
+                        <div id="collapseOne" class="accordion-body collapse out">
+                            <div class="accordion-inner">
+
+
+                                <label>Autre acteur </label>
+                                <input type="text" size="50" name="nom[]"/>
+                                <input type="file" size="50" name="img_other[]"/>
+                                <label>Autre acteur </label>
+                                <input type="text" size="50" name="nom[]"/>
+                                <input type="file" size="50" name="img_other[]"/>
+                                <label>Autre acteur </label>
+                                <input type="text" size="50" name="nom[]"/>
+                                <input type="file" size="50" name="img_other[]"/>
+                                <label>Autre acteur </label>
+                                <input type="text" size="50" name="nom[]"/>
+                                <input type="file" size="50" name="img_other[]"/>
+                                <label>Autre acteur </label>
+                                <input type="text" size="50" name="nom[]"/>
+                                <input type="file" size="50" name="img_other[]"/>
+                            </div>
+                        </div>
+                        </br>
+                        <button type="submit" class="btn" name="ok">Submit</button>
+
+                    </div>
                 </form>
             </div>
         </div>
